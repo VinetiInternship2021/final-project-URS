0008cb844e7945691082004d6b3516403e3582fc
Tigran <t.melikstepanyan@gmail.com>
2021-05-07 11:16:58 +0400

    improved auth

--- Backend/app/controllers/registrations_controller.rb
+++ Backend/app/controllers/registrations_controller.rb
@@ -1,8 +1,3 @@
 class RegistrationsController < Devise::RegistrationsController
-
-    private
-    def sign_up_params
-        params.require(:user).permit(:email,:password,:name)
-    end
 end
   
--- Backend/db/schema.rb
+++ Backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_05_085841) do
+ActiveRecord::Schema.define(version: 2021_05_06_153301) do
 
   create_table "jwt_denylist", force: :cascade do |t|
     t.string "jti", null: false
