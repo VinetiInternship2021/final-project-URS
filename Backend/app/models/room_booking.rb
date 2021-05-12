804cb352c94335b865ee6cb4f9996eae61285d22
Lusine <lusi.erevan@gmail.com>
2021-05-12 13:50:21 +0400

    little fix syntax

--- Backend/app/models/room_booking.rb
+++ Backend/app/models/room_booking.rb
@@ -1,10 +1,8 @@
 class RoomBooking < ApplicationRecord
-  belongs_to :user, dependent: :destroy,
-  belongs_to :room, dependent: :destroy,
+  belongs_to :user, dependent: :destroy
+  belongs_to :room, dependent: :destroy
   has_one :event
   validates_associated :user, :room, :event
   validates :user_id, :room_id, :starts_at, :ends_at, presence: true 
-
-
 
 end
