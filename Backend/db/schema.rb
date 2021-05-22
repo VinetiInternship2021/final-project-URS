a84412955b93df69412439837653823815607199
Tigran <t.melikstepanyan@gmail.com>
2021-05-22 12:51:54 +0400

    changed role type to integer

--- Backend/db/schema.rb
+++ Backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_22_070947) do
+ActiveRecord::Schema.define(version: 2021_05_22_084905) do
 
   create_table "availabilities", force: :cascade do |t|
     t.time "starts_at"
@@ -79,8 +79,20 @@ ActiveRecord::Schema.define(version: 2021_05_22_070947) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-# Could not dump table "users" because of following StandardError
-#   Unknown type 'num' for column 'role'
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.boolean "verified", default: false
+    t.integer "role", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 
   add_foreign_key "availabilities", "rooms"
   add_foreign_key "event_bookings", "events"
