bb62484892e37273b108d697f9dda35b82103cba
Tigran <t.melikstepanyan@gmail.com>
2021-05-24 21:57:36 +0400

    fixed rubocop test-failure

--- Backend/app/controllers/application_controller.rb
+++ Backend/app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
-<<<<<<< HEAD
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   respond_to :json
@@ -9,25 +8,14 @@ class ApplicationController < ActionController::API
   include ActionController::MimeResponds
   include Pundit
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-=======
-  include Pundit
-  before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
-  include ActionController::MimeResponds
-  respond_to :json
-  include ActionController::Helpers
->>>>>>> main
 
   private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :role])
   end
-<<<<<<< HEAD
 
   def user_not_authorized
     render json: "You don't have access", status: 401
   end
-=======
->>>>>>> main
 end
