e6fe6a3b7652305f4869ea978928e33e5cc8930a
Lusine <lusi.erevan@gmail.com>
2021-05-25 12:52:09 +0400

    Reservation, if the room is not reserved yet.

--- Backend/spec/models/room_booking_spec.rb
+++ Backend/spec/models/room_booking_spec.rb
@@ -6,19 +6,4 @@ RSpec.describe RoomBooking, type: :model do
   before(:all) do
     @room_booking = create(:room_booking)
   end
-  # it { should belong_to(:user) }
-
-  # it "is valid with valid attributes" do
-  #   expect(@room_booking).to be_valid
-  # end
-
-  # it "is not valid without a user_id" do
-  # room_booking2 = RoomBooking.new(user: nil)
-  # expect(room_booking2).to_not be_valid
-  # end
-
-  #  it "is not valid without a room_idS" do
-  # room_booking2 = RoomBooking.new(room: nil)
-  # expect(room_booking2).to_not be_valid
-  # end
 end
