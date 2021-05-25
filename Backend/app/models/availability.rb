ac18e25e12a8b8c5d2cdefd30812aecc389b6ef8
Lusine <lusi.erevan@gmail.com>
2021-05-25 16:51:04 +0400

    abailability defoult time from config/settings, faker gem, room controller serilaize fix

--- Backend/app/models/availability.rb
+++ Backend/app/models/availability.rb
@@ -3,10 +3,12 @@
 class Availability < ApplicationRecord
   belongs_to :room, dependent: :destroy
   validate :end_date_after_start_date
+  attribute :starts_at, default: Settings.availability_defaults.starts_at
+  attribute :ends_at, default: Settings.availability_defaults.ends_at
   enum day_of_week: %i[monday tuesday wednesday thursday friday saturday sunday]
   validates :day_of_week, inclusion: { in: day_of_weeks.keys }
   validates_associated :room
-
+ 
   private
 
   def end_date_after_start_date
@@ -16,10 +18,11 @@ class Availability < ApplicationRecord
   end
 
   def starting_time
-    starts_at.strftime('%FT%T')
+    starts_at.strftime('%H:%M')
   end
 
   def ending_time
-    ends_at.strftime('%FT%T')
+    ends_at.strftime('%H:%M')
   end
+
 end
